2c06766aca2bb394db5fb789ffb16cf9da5ac1fa
anaiah <carlodominguez@yahoo.com>
2025-05-21 00:46:56 +0800

    x2

--- assets/app.js
+++ assets/app.js
@@ -31,35 +31,6 @@
                     width: 600,
                     height: 400
                 });
-
-                Ext.getCmp('')
             },
 
-            test:(obj)=>{
-                //console.log('success', obj)
-                const myarg = []
-                myarg.push( obj  )
-
-                //var store = Ext.getStore('poStore');
-                var storeInstance = Ext.data.StoreManager.lookup('poStore')
-
-                storeInstance.loadData(myarg)
-
-                //storeInstance.load()
-
-                console.log(storeInstance)
-
-                if (storeInstance) {
-                    // Get an array of all records
-                    var records = storeInstance.getRange();
-                
-                    // Loop through records
-                    Ext.each(records, function(record) {
-                        console.log('tutssa',record.get('po_number'), record.get('invoice_number'));
-                    });
-                } else{
-                    console.log('boge')
-                }
-
-            }
         });
