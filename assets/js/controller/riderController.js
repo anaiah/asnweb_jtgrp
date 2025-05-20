985490c6b9dd7786e617a3ff86e53cd6afe5d95c
anaiah <carlodominguez@yahoo.com>
2025-05-21 01:21:10 +0800

    x1335

--- assets/js/controller/riderController.js
+++ assets/js/controller/riderController.js
@@ -7,7 +7,63 @@ Ext.define('riderApp.controller.riderController', {
 
     },
 
-    
+    loadPage: (page)=> {
+
+        var start = (page - 1) * asn.pageSize;
+        var end = start + asn.pageSize;
+        var pageData = asn.allData.slice(start, end);
+        
+        asn.ctrlExt.sendData( pageData );
+        
+        asn.currentPage = page;
+
+        asn.ctrlExt.updatePageInfo() //refresh
+
+        console.log('==current page==', asn.currentPage)
+        // Optionally update UI components (like a paging toolbar)
+        // and disable/enable buttons based on page
+        // For example:
+        // Ext.getCmp('prevBtn').setDisabled(page === 1);
+        // Ext.getCmp('nextBtn').setDisabled(end >= allData.length);
+    },
+
+    updatePageInfo:()=> {
+        
+            var start = ( asn.currentPage - 1) * asn.pageSize + 1;
+            var end = Math.min(asn.currentPage * asn.pageSize, asn.allData.length);
+
+        
+        Ext.getCmp('pageInfo').setText('Showing ' + start + ' - ' + end + ' of ' + asn.allData.length);
+    },
+
+    //== load po store / grid
+    sendData:(ydata)=>{
+        console.log('myController.js===== after getpo, loadPO',ydata.length)
+
+        if(ydata) { // if data  not null
+            //====LOAD PO FOR APPROVAL====
+            const storeInstance = Ext.data.StoreManager.lookup('monthlyStore')
+            //storeInstance.removeAll();
+
+            storeInstance.loadData(ydata ) //load ARRAY OF DATA
+            
+            //asn.ctrlExt.updatePageInfo()
+
+            if (storeInstance) {
+                // Get an array of all records
+                var records = storeInstance.getRange();
+
+                // Loop through records
+                Ext.each(records, function(record) {
+                    //console.log('tutssa',record.get('po_number'), record.get('invoice_number'));
+                });
+
+            } else{
+                console.log('boge')
+            }//eif
+            
+        }//eif poData is not null
+    }, 
     
  
     // Your custom function
--- assets/js/main.js
+++ assets/js/main.js
@@ -1242,7 +1242,7 @@ const asn = {
             //replace with 
            // gridMonth.setData( results )
             ////// take ot muna  asn.ctrlExt.loadData(results)
-            asn.loadPage( asn.currentPage ) //load first page
+            asn.ctrlExt.loadPage( asn.currentPage ) //load first page
                     
             //get chart
             asn.getPieChart(util.getCookie('f_dbId'))
@@ -1255,63 +1255,63 @@ const asn = {
     },
 
 
-    loadPage: (page)=> {
+    // loadPage: (page)=> {
 
-        var start = (page - 1) * asn.pageSize;
-        var end = start + asn.pageSize;
-        var pageData = asn.allData.slice(start, end);
+    //     var start = (page - 1) * asn.pageSize;
+    //     var end = start + asn.pageSize;
+    //     var pageData = asn.allData.slice(start, end);
         
-        asn.sendData( pageData );
+    //     asn.sendData( pageData );
         
-        asn.currentPage = page;
+    //     asn.currentPage = page;
 
-        asn.updatePageInfo() //refresh
+    //     asn.updatePageInfo() //refresh
 
-        console.log('==current page==', asn.currentPage)
-        // Optionally update UI components (like a paging toolbar)
-        // and disable/enable buttons based on page
-        // For example:
-        // Ext.getCmp('prevBtn').setDisabled(page === 1);
-        // Ext.getCmp('nextBtn').setDisabled(end >= allData.length);
-    },
+    //     console.log('==current page==', asn.currentPage)
+    //     // Optionally update UI components (like a paging toolbar)
+    //     // and disable/enable buttons based on page
+    //     // For example:
+    //     // Ext.getCmp('prevBtn').setDisabled(page === 1);
+    //     // Ext.getCmp('nextBtn').setDisabled(end >= allData.length);
+    // },
 
-    updatePageInfo:()=> {
+    // updatePageInfo:()=> {
         
-            var start = ( asn.currentPage - 1) * asn.pageSize + 1;
-            var end = Math.min(asn.currentPage * asn.pageSize, asn.allData.length);
+    //         var start = ( asn.currentPage - 1) * asn.pageSize + 1;
+    //         var end = Math.min(asn.currentPage * asn.pageSize, asn.allData.length);
 
         
-        Ext.getCmp('pageInfo').setText('Showing ' + start + ' - ' + end + ' of ' + asn.allData.length);
-    },
+    //     Ext.getCmp('pageInfo').setText('Showing ' + start + ' - ' + end + ' of ' + asn.allData.length);
+    // },
 
-    //== load po store / grid
-    sendData:(ydata)=>{
-        console.log('myController.js===== after getpo, loadPO',ydata.length)
+    // //== load po store / grid
+    // sendData:(ydata)=>{
+    //     console.log('myController.js===== after getpo, loadPO',ydata.length)
 
-        if(ydata) { // if data  not null
-            //====LOAD PO FOR APPROVAL====
-            const storeInstance = Ext.data.StoreManager.lookup('monthlyStore')
-            //storeInstance.removeAll();
+    //     if(ydata) { // if data  not null
+    //         //====LOAD PO FOR APPROVAL====
+    //         const storeInstance = Ext.data.StoreManager.lookup('monthlyStore')
+    //         //storeInstance.removeAll();
 
-            storeInstance.loadData(ydata ) //load ARRAY OF DATA
+    //         storeInstance.loadData(ydata ) //load ARRAY OF DATA
             
-            //asn.ctrlExt.updatePageInfo()
+    //         //asn.ctrlExt.updatePageInfo()
 
-            if (storeInstance) {
-                // Get an array of all records
-                var records = storeInstance.getRange();
+    //         if (storeInstance) {
+    //             // Get an array of all records
+    //             var records = storeInstance.getRange();
 
-                // Loop through records
-                Ext.each(records, function(record) {
-                    //console.log('tutssa',record.get('po_number'), record.get('invoice_number'));
-                });
+    //             // Loop through records
+    //             Ext.each(records, function(record) {
+    //                 //console.log('tutssa',record.get('po_number'), record.get('invoice_number'));
+    //             });
 
-            } else{
-                console.log('boge')
-            }//eif
+    //         } else{
+    //             console.log('boge')
+    //         }//eif
             
-        }//eif poData is not null
-    },
+    //     }//eif poData is not null
+    // },
 
 
     //===save to localstorage
@@ -1661,7 +1661,7 @@ Ext.onReady(function(){
 
     
     // Get the controller
-    //asn.ctrlExt = asn.appExt.getController('riderController');
+    asn.ctrlExt = asn.appExt.getController('riderController');
        
     asn.getMonthlyTransaction(util.getCookie('f_dbId'))
     
