f00cc02e39bfa8f1c6b88acb25b4b070aea91813
anaiah <carlodominguez@yahoo.com>
2025-05-21 12:42:07 +0800

    fff

--- assets/coordapp.js
+++ assets/coordapp.js
@@ -1,50 +1,41 @@
-Ext.require([
-    'Ext.grid.*',
-    'Ext.data.*',
-    'Ext.panel.*',
-    'Ext.form.field.Number',
-    'Ext.form.field.Date',
-    'Ext.tip.QuickTipManager'
-]);
-  
-  //load ext
-        Ext.application({
-            name: 'MyApp',
-            appFolder: '/html/assets/js',
-            //models: ['monthlyModel'],
-            stores: 
-            [
-                'locationStore',
-                'riderStore'
-                // 'coordStore',
-                // 'headStore',
-                // 'opmgrStore'
-            ],
+ //load ext
+Ext.application({
+    name: 'MyApp',
+    appFolder: '/html/assets/js',
+    //models: ['monthlyModel'],
+    stores: 
+    [
+        'locationStore',
+        'riderStore'
+        // 'coordStore',
+        // 'headStore',
+        // 'opmgrStore'
+    ],
 
-            controllers:
-            [   
-                'coordController',
-                // 'coordCtrl',
-                // 'headCtrl',
-                // 'opmgrCtrl',
-            ],
+    controllers:
+    [   
+        'coordController',
+        // 'coordCtrl',
+        // 'headCtrl',
+        // 'opmgrCtrl',
+    ],
+
+    // Launch method - called when app is ready
+    launch: function() {
         
-            // Launch method - called when app is ready
-            launch: function() {
-                
-                console.log('====Ext.app 4.2 Launch() ====',)
-                MyApp.app = this    
+        console.log('====Ext.app 4.2 Launch() ====',)
+        MyApp.app = this    
 
-                // var locGrid = Ext.create('MyApp.view.locationGrid', {
-                //     renderTo: 'grid_month',
-                //     width: 500,
-                //     height: 300
-                // });
-                // var rideGrid = Ext.create('MyApp.view.riderGrid', {
-                //     renderTo: 'rider_grid',
-                //     width: 500,
-                //     height: 400
-                // });
-
-            },
+        var locGrid = Ext.create('MyApp.view.locationGrid', {
+            renderTo: 'grid_month',
+            width: 500,
+            height: 300
         });
+        var rideGrid = Ext.create('MyApp.view.riderGrid', {
+            renderTo: 'rider_grid',
+            width: 500,
+            height: 400
+        });
+
+    },
+});
