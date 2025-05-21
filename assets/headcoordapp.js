b639e0b1f1aed8bf55bc27b3c9b29f68e09dd612
anaiah <carlodominguez@yahoo.com>
2025-05-21 23:43:13 +0800

    headcoord

---
+++ assets/headcoordapp.js
@@ -0,0 +1,42 @@
+ //load ext
+Ext.application({
+    name: 'MyApp',
+    appFolder: '/html/assets/js',
+    //models: ['monthlyModel'],
+    stores: 
+    [
+        'locationStore',
+        'riderStore'
+        // 'coordStore',
+        // 'headStore',
+        // 'opmgrStore'
+    ],
+
+    controllers:
+    [   
+        'coordController',
+        // 'coordCtrl',
+        // 'headCtrl',
+        // 'opmgrCtrl',
+    ],
+
+    // Launch method - called when app is ready
+    launch: function() {
+        
+        console.log('====Ext.app 4.2 Launch() ====',)
+        MyApp.app = this    
+
+        /*
+        var locGrid = Ext.create('MyApp.view.locationGrid', {
+            renderTo: 'grid_month',
+            width: 500,
+            height: 300
+        });
+        var rideGrid = Ext.create('MyApp.view.riderGrid', {
+            renderTo: 'rider_grid',
+            width: 500,
+            height: 400
+        });
+        */
+    },
+});
