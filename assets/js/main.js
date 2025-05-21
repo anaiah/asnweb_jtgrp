065332d12c4caa3dbdb6760f445e967b42487447
anaiah <carlodominguez@yahoo.com>
2025-05-21 11:42:07 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -767,7 +767,7 @@ const asn = {
             const targetId = this.getAttribute('href');
             console.log('Clicked link:', targetId);
             
-            if (targetId || targetId.startsWith('#')) {
+            if (targetId && targetId.startsWith('#')) {
               document.querySelector(targetId).scrollIntoView({ behavior: 'smooth' });
             }
             
