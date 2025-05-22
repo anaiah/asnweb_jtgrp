0edf51f177f54dc29764b9f72ca0d81f1a644b9f
anaiah <carlodominguez@yahoo.com>
2025-05-23 02:04:09 +0800

    Update opmgrGrid.js

--- assets/js/view/opmgrGrid.js
+++ assets/js/view/opmgrGrid.js
@@ -6,15 +6,47 @@ Ext.define('MyApp.view.opmgrGrid' ,{
     store: Ext.data.StoreManager.lookup('opmgrStore'), // your storeId
     width: 700,
     height: 400,
+
+    plugins: [{
+        ptype: 'gridsummary'
+    }],
+
     columns: [
         { text: 'Region', dataIndex: 'region', flex: 1 },
         { text: 'Area', dataIndex: 'area', flex: 1 },
-        { text: 'Parcel', dataIndex: 'parcel', width: 80 },
-        { text: 'Amount', dataIndex: 'amount', width: 100, renderer: Ext.util.Format.usMoney },
-        { text: 'Delivered', dataIndex: 'parcel_delivered', width: 80 },
-        { text: 'Remitted', dataIndex: 'amount_remitted', width: 100, renderer: Ext.util.Format.usMoney },
-        { text: '% Qty', dataIndex: 'qty_pct', width: 80, renderer: function(value) { return Ext.Number.toFixed(value * 100, 1) + '%'; } }
+        { text: 'Parcel', dataIndex: 'parcel', width: 80, 
+        summaryType: 'sum', 
+        renderer: Ext.util.Format.numberRenderer('0') 
+        },
+        { text: 'Amount', dataIndex: 'amount', width: 100, 
+        summaryType: 'sum', 
+        renderer: Ext.util.Format.usMoney
+        },
+        { text: 'Delivered', dataIndex: 'parcel_delivered', width: 80, 
+        summaryType: 'sum'
+        },
+        { text: 'Remitted', dataIndex: 'amount_remitted', width: 100, 
+        summaryType: 'sum',
+        renderer: Ext.util.Format.usMoney
+        },
+        { text: '% Qty', dataIndex: 'qty_pct', width: 80,
+        renderer: function(value) { return Ext.Number.toFixed(value * 100, 1) + '%'; },
+        summaryType: function(records, field) {
+            // For custom summary, e.g., average
+            var total = 0;
+            var count = 0;
+            Ext.each(records, function(r) {
+                var val = r.get(field);
+                if (Ext.isNumber(val)) {
+                    total += val;
+                    count++;
+                }
+            });
+            return count > 0 ? Ext.Number.toFixed(total / count, 2) : 0;
+        }
+        }
     ],
+
     viewConfig: {
         stripeRows: true,
         emptyText: 'No data available',
