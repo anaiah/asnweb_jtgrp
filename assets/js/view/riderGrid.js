b2dd0a27f286b10c1a92e11a9c541d1141640e3e
anaiah <carlodominguez@yahoo.com>
2025-07-05 22:04:33 +0800

    Update riderGrid.js

--- assets/js/view/riderGrid.js
+++ assets/js/view/riderGrid.js
@@ -28,7 +28,8 @@ Ext.define('MyApp.view.riderGrid' ,{
     listeners:{
         viewready: function(grid) {
             console.log('@@@riders grid viewready');
-            
+             var store = grid.getStore()
+             store.sort('qty', 'DESC');
            
         },//end viewready
         cellmousedown: function(view, cell, cellIdx, record, row, rowIdx, eOpts){
