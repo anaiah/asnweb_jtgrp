1c8cd6451d9876b49a201c9c0d93c14a1ba41661
anaiah <carlodominguez@yahoo.com>
2025-07-18 23:23:27 +0800

    Update ipconfig.js

--- assets/js/ipconfig.js
+++ assets/js/ipconfig.js
@@ -1,5 +1,5 @@
 
 //const myIp = "http://10.202.213.221:10000"
 //const myIp  = "https://esndp-gis-jku4q.ondigitalocean.app"
-const myIp  = "https://asianowapp-jtx-lqum6.ondigitalocean.app"
-//const myIp= "https://asn-jtgrp-api.onrender.com"
+//const myIp  = "https://asianowapp-jtx-lqum6.ondigitalocean.app"
+const myIp= "https://asn-jtgrp-api.onrender.com"
